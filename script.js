5c0437701bba3b873187ffe35600e599f1380349
Prathamesh Kadam <146866950+PrathameshKadam-07@users.noreply.github.com>
2026-04-04 21:57:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function typeEffect() {
     if (!deleting) i = (i + 1) % words.length;
   }
 
-  setTimeout(typeEffect, deleting ?  130 : 150);
+  setTimeout(typeEffect, deleting ?  130 : 90);
 }
 typeEffect();
 
